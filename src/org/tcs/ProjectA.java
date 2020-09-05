ea7e7587534d9d6f8d2de387a40b8d0a186575de
metgunaseelan <metgunaseelan@gmail.com>
2020-09-06 00:39:41 +0530

    To master

---
+++ src/org/tcs/ProjectA.java
@@ -0,0 +1,22 @@
+package org.tcs;
+
+public class ProjectA {
+	
+	private void m1() {
+		System.out.println("Method 1");
+	}
+
+	private void m2() {
+		System.out.println("Method 2");
+
+	}
+
+	public static void main(String[] args) {
+		ProjectA p = new ProjectA();
+		
+		p.m1();
+		p.m2();
+		
+	}
+
+}
