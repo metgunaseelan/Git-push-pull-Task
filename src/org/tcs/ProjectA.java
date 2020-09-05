5bacb5a1bdfff0c8e4a06dfbf1c58234f7a3b19a
metgunaseelan <metgunaseelan@gmail.com>
2020-09-06 01:15:33 +0530

    To Branch-B

--- src/org/tcs/ProjectA.java
+++ src/org/tcs/ProjectA.java
@@ -2,20 +2,12 @@ package org.tcs;
 
 public class ProjectA {
 	
-	private void m1() {
-		System.out.println("Method 1");
+	private ProjectA() {
+		System.out.println("Default constructor");
 	}
 
-	private void m2() {
-		System.out.println("Method 2");
-
-	}
-
-	public static void main(String[] args) {
-		ProjectA p = new ProjectA();
-		
-		p.m1();
-		p.m2();
+	private ProjectA(int a) {
+		System.out.println("Parameterised Constructor");
 		
 	}
 
